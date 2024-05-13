710719e645046069afa72557e9c72ce86ce820cc
mdscr3 <160481957+mdscr3@users.noreply.github.com>
2024-05-13 11:13:04 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,30 +1,18 @@
 input.onButtonPressed(Button.A, function () {
-    voeding = 0
+    knuffel += 5
 })
 input.onButtonPressed(Button.B, function () {
     knuffel += 5
 })
-let voeding = 0
 let knuffel = 20
-voeding = 20
+let voeding = 20
 let dood = 0
 loops.everyInterval(1000, function () {
     voeding += -1
     knuffel += -1
 })
 basic.forever(function () {
-    if (knuffel > 10) {
-        basic.showIcon(IconNames.Happy)
-    }
-    if (knuffel < 10 && knuffel > 0) {
-        basic.showIcon(IconNames.Sad)
-    }
-    if (knuffel < 0) {
-        basic.showIcon(IconNames.Asleep)
-    }
-})
-basic.forever(function () {
-    if (voeding > 10) {
+    if (knuffel > 10 && voeding > 10) {
         basic.showLeds(`
             . . . . .
             . # . # .
@@ -33,7 +21,7 @@ basic.forever(function () {
             . # # # .
             `)
     }
-    if (voeding < 10 && voeding > 0) {
+    if (knuffel < 10 && knuffel > 0 && (voeding < 10 && voeding > 0)) {
         basic.showLeds(`
             . . . . .
             . # . # .
@@ -42,7 +30,7 @@ basic.forever(function () {
             # . . . #
             `)
     }
-    if (voeding < 0) {
+    if (knuffel < 0 && voeding < 0) {
         basic.showLeds(`
             . . . . .
             # # . # #
